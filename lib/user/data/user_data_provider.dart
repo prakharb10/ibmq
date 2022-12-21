49dcd435d3d677d94cb6de6f6118714b5096b973
Prakhar Bhatnagar <42675093+prakharb10@users.noreply.github.com>
2022-12-21 15:53:04 +0530

    switched to `CookieJar`

--- lib/user/data/user_data_provider.dart
+++ lib/user/data/user_data_provider.dart
@@ -3,19 +3,18 @@ import 'dart:developer';
 import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
-import 'package:path_provider/path_provider.dart';
 
 class UserDataProvider {
   final Dio dio = Dio(BaseOptions(
     baseUrl: "https://auth.quantum-computing.ibm.com/api",
   ));
+  final CookieJar cookieJar = CookieJar();
 
-  UserDataProvider();
+  UserDataProvider() {
+    dio.interceptors.add(CookieManager(cookieJar));
+  }
 
   Future<Response> tokenLogin(String token) async {
-    final tempDir = await getTemporaryDirectory();
-    final cookieJar = PersistCookieJar(storage: FileStorage(tempDir.path));
-    dio.interceptors.add(CookieManager(cookieJar));
     try {
       final response = await dio.post(
         '/users/loginWithToken',
@@ -29,9 +28,6 @@ class UserDataProvider {
   }
 
   Future<Response> getUserData() async {
-    final tempDir = await getTemporaryDirectory();
-    final cookieJar = PersistCookieJar(storage: FileStorage(tempDir.path));
-    dio.interceptors.add(CookieManager(cookieJar));
     try {
       final response = await dio.get('/users/me');
       return response;
