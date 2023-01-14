9c1e125a676756645b718cbab5866e417cb472a3
Prakhar Bhatnagar <42675093+prakharb10@users.noreply.github.com>
2023-01-14 20:12:54 +0530

    added job timeline

--- lib/jobs/job/job_page.dart
+++ lib/jobs/job/job_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ibmq/jobs/job/cubit/job_cubit.dart';
+import 'package:ibmq/jobs/job/widgets/job_timeline.dart';
 import 'package:intl/intl.dart';
 
 class JobPage extends StatefulWidget {
@@ -42,89 +43,112 @@ class _JobPageState extends State<JobPage> {
             appBar: AppBar(
               title: Text(job.id),
             ),
-            body: Card(
-              margin: const EdgeInsets.all(8),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      "Details",
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+            body: ListView(
+              children: [
+                if (job.infoQueue != null)
+                  Column(
+                    children: [
+                      Text("Position in queue: ${job.infoQueue!.position}"),
+                      Text(
+                          "Estimated start time: ${DateFormat.yMMMd().add_jm().format(job.infoQueue!.estimatedStartTime.toLocal())}"),
+                      Text(
+                          "Estimated completion time: ${DateFormat.yMMMd().add_jm().format(job.infoQueue!.estimatedCompleteTime.toLocal())}"),
+                    ],
+                  ),
+                Card(
+                  margin: const EdgeInsets.all(8),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
                       children: [
-                        Column(
+                        Text(
+                          "Details",
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            Text(
-                                "${job.timePerStep.completed.difference(job.timePerStep.creating).inSeconds.toString()}s"),
-                            Text(
-                              "Total completion time",
-                              style: Theme.of(context).textTheme.labelMedium,
+                            if (job.timePerStep.completed != null)
+                              Column(
+                                children: [
+                                  Text(
+                                      "${job.timePerStep.completed?.difference(job.timePerStep.creating!).inSeconds.toString()}s"),
+                                  Text(
+                                    "Total completion time",
+                                    style:
+                                        Theme.of(context).textTheme.labelMedium,
+                                  )
+                                ],
+                              ),
+                            Column(
+                              children: [
+                                Text(job.backend.name),
+                                Text(
+                                  "Compute resource",
+                                  style:
+                                      Theme.of(context).textTheme.labelMedium,
+                                )
+                              ],
                             )
                           ],
                         ),
-                        Column(
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            Text(job.backend.name),
+                            const Text("Created on"),
                             Text(
-                              "Compute resource",
-                              style: Theme.of(context).textTheme.labelMedium,
-                            )
+                              DateFormat.yMMMd()
+                                  .add_jm()
+                                  .format(job.creationDate.toLocal()),
+                            ),
                           ],
-                        )
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        const Text("Created on"),
-                        Text(
-                          DateFormat.yMMMd()
-                              .add_jm()
-                              .format(job.creationDate.toLocal()),
+                        ),
+                        if (job.timePerStep.queued != null)
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              const Text("Sent to queue"),
+                              Text(
+                                DateFormat.yMMMd()
+                                    .add_jm()
+                                    .format(job.timePerStep.queued!.toLocal()),
+                              ),
+                            ],
+                          ),
+                        if (job.runMode != null)
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              const Text("Run Mode"),
+                              Text(job.runMode!),
+                            ],
+                          ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            const Text("# of shots"),
+                            Text(job.summaryData.summary.qobjConfig.shots
+                                .toString()),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            const Text("# of circuits"),
+                            Text(
+                                job.summaryData.summary.numCircuits.toString()),
+                          ],
                         ),
                       ],
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        const Text("Sent to queue"),
-                        Text(
-                          DateFormat.yMMMd()
-                              .add_jm()
-                              .format(job.timePerStep.queued.toLocal()),
-                        ),
-                      ],
-                    ),
-                    if (job.runMode != null)
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          const Text("Run Mode"),
-                          Text(job.runMode!),
-                        ],
-                      ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        const Text("# of shots"),
-                        Text(job.summaryData.summary.qobjConfig.shots
-                            .toString()),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        const Text("# of circuits"),
-                        Text(job.summaryData.summary.numCircuits.toString()),
-                      ],
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+                JobTimeline(
+                  timePerStep: job.timePerStep,
+                  resultTime: job.summaryData.resultTime,
+                ),
+              ],
             ),
           );
         }
